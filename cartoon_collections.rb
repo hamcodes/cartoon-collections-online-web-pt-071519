e8a11b2c07d0d1fe4785463cc736a2462a19038c
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-23 17:38:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.any? do {|x| x.length > 4}# Your code here
+  array.any? do |x| x.length > 4# Your code here
 end
 
 def find_the_cheese# code an argument here
