cb1b63476012b04f6b11c85758f5cc1be2401d09
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-23 17:38:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ end
 def long_planeteer_calls(array)# code an argument here
   array.any? do |x| x.length > 4# Your code here
 end
+end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
