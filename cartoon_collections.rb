2e62d3fbca7a2d4243da4799ce93edda153b730a
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-23 17:33:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(names)
   end
 end
 
-def summon_captain_planet(array)# code an argument here
-  array.map {|array| array.upcase + "!"}
+def summon_captain_planet(array)
+  array.map { |item| item.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
