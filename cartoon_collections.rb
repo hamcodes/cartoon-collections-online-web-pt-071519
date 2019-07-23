a19bd15c7fa7eefcabe4dfcd65f780a28cceb35c
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-23 17:24:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,11 +3,7 @@ def roll_call_dwarves(names)
   puts "#{index + 1}. #{name}"
   end
 end
-# def roll_call_dwarves(names)
-#   names.each_with_index do |name, index|
-#     puts "#{index + 1}. #{name}"
-#   end
-# end
+
 def summon_captain_planet# code an argument here
   # Your code here
 end
